28e0d8325b599a20287ed9298babde454fb33aaa
David Carroll <david@bvcms.com>
2014-07-16 12:26:00 -0500

    Working On RegistrationSearch

--- CmsWeb/Areas/Search/Controllers/RegistrationSearchController.cs
+++ CmsWeb/Areas/Search/Controllers/RegistrationSearchController.cs
@@ -4,71 +4,32 @@ using UtilityExtensions;
 
 namespace CmsWeb.Areas.Search.Controllers
 {
-    [RouteArea("Search", AreaPrefix = "Registrations"), Route("{action}/{id?}")]
+    [RouteArea("Search", AreaPrefix = "RegistrationSearch"), Route("{action}/{id?}")]
     public class RegistrationSearchController : CmsStaffController
     {
-        [HttpGet]
+        [HttpGet, Route("~/RegistrationSearch")]
         public ActionResult Index()
         {
             Response.NoCache();
-            var m = new ContactSearchModel();
-            m.Pager.Set("/Registrations/Results");
+            var m = new RegistrationSearchModel();
+            m.Pager.Set("/RegistrationSearch/Results");
 
             m.GetFromSession();
             return View(m);
         }
         [HttpPost, Route("Results/{page?}/{size?}/{sort?}/{dir?}")]
-        public ActionResult Results(int? page, int? size, string sort, string dir, ContactSearchModel m)
+        public ActionResult Results(int? page, int? size, string sort, string dir, RegistrationSearchModel m)
         {
-            m.Pager.Set("/ContactSearch2/Results", page, size, sort, dir);
+            m.Pager.Set("/RegistrationSearch/Results", page, size, sort, dir);
             m.SaveToSession();
             return View(m);
         }
         [HttpPost]
         public ActionResult Clear()
         {
-            var m = new ContactSearchModel();
+            var m = new RegistrationSearchModel();
             m.ClearSession();
-            return Redirect("/ContactSearch2");
-        }
-        [HttpPost]
-        public ActionResult ConvertToQuery(ContactSearchModel m)
-        {
-            var gid = m.ConvertToQuery();
-            return Redirect("/Query/{0}".Fmt(gid));
-        }
-        [HttpPost]
-        public ActionResult ContactTypeQuery(int id)
-        {
-            var gid = ContactSearchModel.ContactTypeQuery(id);
-            return Redirect("/Query/{0}".Fmt(gid));
-        }
-        [HttpPost]
-        public ActionResult ContactorSummary(ContactSearchModel m)
-        {
-            var q = m.ContactorSummary();
-            return View(q);
-        }
-        [HttpPost]
-        public ActionResult ContactSummary(ContactSearchModel m)
-        {
-            var q = m.ContactSummary();
-            return View(q);
-        }
-
-        [HttpPost]
-        public ActionResult ContactTypeTotals(ContactSearchModel m)
-        {
-            ViewBag.candelete = m.CanDeleteTotal();
-            var q = m.ContactTypeTotals();
-            return View(q);
-        }
-
-        [Authorize(Roles = "Developer")]
-        public ActionResult DeleteContactsForType(int id)
-        {
-            ContactSearchModel.DeleteContactsForType(id);
-            return Redirect("/ContactSearch/ContactTypeTotals");
+            return Redirect("/RegistrationSearch");
         }
     }
 }
